9054e0690a6f85af42492860cc1003701e219903
Audrow Nash <audrow@hey.com>
2022-03-13 21:13:10 -0700

    Add basic person card
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ components/PersonCard/PersonCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export interface PersonCardProps {
+  /**
+   *  The full name of the person.
+   */
+  name: string
+  /**
+   *  The phone number of the person.
+   */
+  phoneNumber?: string
+  /**
+   *  The email address of the person.
+   */
+  email?: string
+  /**
+   *  The birthday of the person in the format of MM/DD/YYYY
+   */
+  birthday?: string
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const PersonCard: React.FC<PersonCardProps> = ({
+  name,
+  phoneNumber,
+  email,
+  birthday,
+}) => {
+  return (
+    <>
+      <p>
+        Name: {name}
+      </p>
+      {
+        phoneNumber &&
+        <p>
+          Phone: {phoneNumber}
+        </p>
+      }
+      {
+        email &&
+        <p>
+          Email: {email}
+        </p>
+      }
+      {
+        birthday &&
+        <p>
+          Birthday: {birthday}
+        </p>
+      }
+    </>
+  )
+}
